c74cea7c3334e5efca08caaaed9c1787d81c2b51
Leandro M Mohr <leandro.mohr@mirumagency.com>
2019-10-25 20:23:06 -0300

    [ + ] Include background-color for all pages

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,13 @@ export default {
 </script>
 
 <style lang="scss">
-
+body{
+  background-color: #8ac8d5;
+  background-image:
+  radial-gradient(rgba(76,139,152,1), rgba(138,201,214,.05) 200px, transparent 400px),
+  radial-gradient(rgba(76,139,152,1), rgba(255,255,255,.1) 300px, transparent 400px),
+  radial-gradient(rgba(76,139,152,1), rgba(138,201,214,.1) 400px, transparent 400px);
+  background-size: 1000px 500px, 700px 500px, 550px 500px;
+  background-position: 0 -200px, 300px -100px, 700px -200px;
+}
 </style>
